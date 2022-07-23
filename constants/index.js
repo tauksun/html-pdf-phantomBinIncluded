c70935db85a14bd45742e68d03d712b32f45fbee
tauksun <tauksun@gmail.com>
2022-07-23 06:45:01 +0000

    Initialized

---
+++ constants/index.js
@@ -0,0 +1,4 @@
+const path = require("path");
+module.exports = {
+  "phantomjs-binary": path.join(__dirname, "../phantom-binary/phantomjs"),
+};
---
+++ install.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const path = require("path");
+
+// Check platform
+const platform = process.env.platform || process.platform;
+
+// Get file extension
+const fileExtension = platform === "win32" ? ".exe" : "";
+console.log(`Making phantomjs binary executable for all users on ${platform}`);
+
+// Give Permission
+try {
+  const binaryPath = path.join(
+    __dirname,
+    `./phantom-binary/phantomjs${fileExtension}`
+  );
+  fs.chmodSync(binaryPath);
+  console.log("Successfully made phantomjs executable");
+} catch (error) {
+  console.log(
+    "Error occured while making phantomjs binary executable :\n",
+    error
+  );
+}
